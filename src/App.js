da0ce1a8e72ca0b04aafd1da71027d91fc8bc6e9
Ananya Kaushik <home.ananya@gmail.com>
2021-03-25 06:20:03 -0700

    add styling to card and background

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
 import Flashcard from './components/Flashcard/Flashcard';
 
 function App() {
   return (
     <div className="App">
+      {/* Display flashcard with the Word */}
       <Flashcard />
     </div>
   );
--- src/components/Flashcard/Flashcard.js
+++ src/components/Flashcard/Flashcard.js
@@ -4,10 +4,13 @@ import classes from './Flashcard.module.css';
 class Flashcard extends Component {
     render() {
         return(
+            // Card Component for flashcard
             <div class={`card ${classes.CardComp}`}>
-                <div class="card-body">
-                    <h5 class="card-title">Word:</h5>
-                    <p class="card-text">Hello</p>
+                <div class={`card-body ${classes.CardBody}`}>
+                    {/* Title */}
+                    <h5 class={`card-title ${classes.CardTitle}`}>WORD:</h5>
+                    {/* Word displayed for Song Association Game */}
+                    <p class={`card-text ${classes.CardText}`}>DREAM</p>
                 </div>
             </div>
         );
