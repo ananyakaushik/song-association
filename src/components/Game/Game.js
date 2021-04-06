9e471bc5a26b5fec14399055f269d835b9688659
Ananya Kaushik <home.ananya@gmail.com>
2021-04-06 00:20:23 -0700

    add start/restart functionality and set timer for words

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -10,17 +10,48 @@ class Game extends Component {
             // placeholder word info
             words: ['one', 'two', 'three', 'four', 'five',
                     'six', 'seven', 'eight', 'nine', 'ten',
-                    'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen']
+                    'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen'],
+            currentWord: "",
+            playing: false
         }
+    }
+
+    // Toggle playing of game - start and restart
+    togglePlay = () => {
+        //  Change state of play
+        const newPlayState = !this.state.playing;
+        this.setState({ playing: newPlayState});
+
+        if (newPlayState) {
+            // Start game and call wordTimer to time 15 words
+            setTimeout(() => this.wordTimer(0), 1000);
+
+        } else {
+            // Restart game by changing current word to empty string
+            this.setState({ currentWord: "" });
+        }
+    }
+
+    // Wait 10 seconds before calling next word in Game
+    wordTimer = index => {
+        // Only iterate through 15 words
+        if (index < 15) {
+            // Change current word and wait 10s for next word
+            this.setState({ currentWord: this.state.words[index] })
+            setTimeout(() => this.wordTimer(index + 1), 10000);
+        }
+
     }
 
     render() {
         return(
             // Card Component for flashcard
             <div className={classes.Game}>
-                <Flashcard word={this.state.words.length ? this.state.words[0].toUpperCase() : "CLICK START"}/>
+                {/* <Flashcard word={this.state.words.length ? this.state.words[0].toUpperCase() : "CLICK START"}/> */}
+                <Flashcard word={this.state.currentWord ? this.state.currentWord.toUpperCase() : "CLICK START"}/>
                 <br />
-                <PlayButton />
+                {/* Start or Restart game with button */}
+                <PlayButton togglePlay={this.togglePlay} playing={this.state.playing} />
             </div>
         );
     }
--- src/components/PlayButton/PlayButton.js
+++ src/components/PlayButton/PlayButton.js
@@ -6,8 +6,8 @@ class PlayButton extends Component {
     render() {
         return(
             // Card Component for flashcard
-            <button className={classes.Button}>
-                START
+            <button className={classes.Button} onClick={this.props.togglePlay} >
+                {this.props.playing? "RESTART" : "START"}
             </button>
         );
     }
